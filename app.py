1b98bd328b3a9a574ce65ed9e71e413fed7e487a
FreePDF-convert <free0pdf0converter@gmail.com>
2025-06-15 21:18:41 +0200

    Añadido bloque para ejecutar en Render

--- app.py
+++ app.py
@@ -50,3 +50,8 @@ def index():
         return send_file(pdf_path, as_attachment=True)
 
     return render_template('index.html')
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
